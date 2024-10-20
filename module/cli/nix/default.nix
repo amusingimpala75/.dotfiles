f7c9bd9bd47b9ba2a662aa78a1692a5e4b5838f4
amusingimpala75 <69653100+amusingimpala75@users.noreply.github.com>
2024-10-20 03:51:51 +0000

    Fix reload-no

--- module/cli/nix/default.nix
+++ module/cli/nix/default.nix
@@ -3,7 +3,7 @@
   home.shellAliases = {
     reload-hm = "home-manager switch -b backup --flake ${dotfilesDir}#${username}_${hostname}";
     reload-nd = if pkgs.stdenv.isDarwin then "darwin-rebuild switch --flake ${dotfilesDir}" else "echo 'Did you mean reload-no?'";
-    reload-no = if pkgs.stdenv.isLinux  then "nixos-rebuild  switch --flake ${dotfilesDir}" else "echo 'Did you mean reload-nd?'";
+    reload-no = if pkgs.stdenv.isLinux  then "sudo nixos-rebuild switch --flake ${dotfilesDir}" else "echo 'Did you mean reload-nd?'";
     reload-config = if pkgs.stdenv.isLinux then "reload-no && reload-hm" else "reload-nd && reload-hm";
   };
 }
