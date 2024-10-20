0e14334410a6e514930878e7df71fcb2703fc97e
amusingimpala75 <69653100+amusingimpala75@users.noreply.github.com>
2024-10-20 03:52:28 +0000

    Change to woodland theme

--- user/murrayle23/settings.nix
+++ user/murrayle23/settings.nix
@@ -1,7 +1,7 @@
 {
   opacity = 0.8;
   font = import ../../module/font/iosevka;
-  theme = import ../../module/theme/generated/gruvbox-dark-medium;
+  theme = import ../../module/theme/generated/woodland;
   git-email = "69653100+amusingimpala75@users.noreply.github.com";
   git-username = "amusingimpala75";
 }
