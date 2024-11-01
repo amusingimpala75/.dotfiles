88f5b4997830e2e91dbda04f5912090989937117
amusingimpala75 <69653100+amusingimpala75@users.noreply.github.com>
2024-11-01 00:14:01 +0000

    Adjust transparency and hide menubar on non-macOS systems

--- user/lukemurray/settings.nix
+++ user/lukemurray/settings.nix
@@ -1,5 +1,5 @@
 {
-  opacity = 0.8;
+  opacity = 0.9;
   font = import ../../module/font/iosevka;
   theme = import ../../module/theme/generated/woodland;
   git-email = "69653100+amusingimpala75@users.noreply.github.com";
