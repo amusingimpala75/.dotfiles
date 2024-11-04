a8ee4093085a0a141851fa7a1a174aebd36b56eb
amusingimpala75 <69653100+amusingimpala75@users.noreply.github.com>
2024-11-03 22:11:50 -0500

    Fix installer errors

--- install.sh
+++ install.sh
@@ -2,12 +2,12 @@
 
 DOTDIR="$HOME/.dotfiles"
 
-nix run nixpkgs#git -- clone https://github.com/amusingimpala75/.dotfiles "$DOTDIR"
+# nix run nixpkgs#git -- clone https://github.com/amusingimpala75/.dotfiles "$DOTDIR"
 
-if [ $(uname) = "Darwin" ]; then
-    nix run nix-darwin/master -- switch --flake "$DOTDIR"
+if [ "$(uname)" = "Darwin" ]; then
+    nix run nix-darwin/master --extra-experimental-features "nix-command flakes" -- switch --flake "$DOTDIR"
 else
     sudo nixos-rebuild switch --flake "$DOTDIR"
 fi
 
-nix run home-manager/master -- switch --flake "$DOTDIR"#"$(id -un)_$(hostname -s)"
+nix run home-manager/master --extra-experimental-features "nix-command flakes" -- switch --flake "$DOTDIR"#"$(id -un)_$(hostname -s)"
