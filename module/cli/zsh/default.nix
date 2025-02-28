35cb52743279bba623d9f07d939dc307ebd607ab
amusingimpala75 <69653100+amusingimpala75@users.noreply.github.com>
2025-02-28 20:20:27 +0000

    Update zsh prompt to account for 'nix shell' shells

--- module/cli/zsh/default.nix
+++ module/cli/zsh/default.nix
@@ -5,12 +5,17 @@
     dotDir = ".config/zsh";
     initExtra = ''
       function precmd {
-      if [[ -z "$IN_NIX_SHELL" ]]
-      then
-      _PROMPT_NIX_SHELL="";
-      else
-      _PROMPT_NIX_SHELL=" (dev)";
-      fi
+        if echo "$PATH" | grep -q "/nix/store";
+        then
+          if [[ -z "$IN_NIX_SHELL" ]]
+          then
+            _PROMPT_NIX_SHELL=" (shell)"
+          else
+            _PROMPT_NIX_SHELL=" (dev)"
+          fi
+        else
+          _PROMPT_NIX_SHELL=""
+        fi
       }
       setopt prompt_subst
       export PROMPT='%n@%U%m%u''${_PROMPT_NIX_SHELL}> '
