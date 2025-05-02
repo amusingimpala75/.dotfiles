eed6055eaa31eaea54ad604a42c33bf967f47dd1
amusingimpala75 <69653100+amusingimpala75@users.noreply.github.com>
2025-05-02 21:23:47 +0000

    emacs: remove workaround (integrated into nixpkgs)

--- overlays/default.nix
+++ overlays/default.nix
@@ -27,8 +27,4 @@ final: prev: {
   my.schemes = prev.callPackage ../packages/schemes.nix { };
   my.wallpapers = final.callPackage ../packages/wallpapers.nix { };
   my.base16-generators.emacs = final.callPackage ../packages/base16-emacs.nix { };
-
-  emacs = prev.emacs.override {
-    withNativeCompilation = !prev.stdenv.isDarwin;
-  };
 }
