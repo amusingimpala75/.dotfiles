d47063fce8fbd9e6f0ff34ede3b930e0994292ea
amusingimpala75 <69653100+amusingimpala75@users.noreply.github.com>
2025-05-10 18:20:14 -0400

    nix: explicitly enable sandbox

--- modules/shared/nix.nix
+++ modules/shared/nix.nix
@@ -9,6 +9,8 @@ in {
     settings = {
       experimental-features = [ "nix-command" "flakes" ];
       flake-registry = "";
+    } // lib.optionalAttrs (!isHomeManager) {
+      sandbox = true;
     };
     package = pkgs.nixVersions.nix_2_28;
     registry = lib.mapAttrs (_: flake: { inherit flake; }) inputs;
