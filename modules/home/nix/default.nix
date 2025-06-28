43b5bfaba3e5efe69d287dec7a3bd928a4279cae
amusingimpala75 <69653100+amusingimpala75@users.noreply.github.com>
2025-06-28 12:26:59 -0700

    nh: remove sudo from invocation

--- modules/home/nix/default.nix
+++ modules/home/nix/default.nix
@@ -18,8 +18,8 @@ in {
       shellAliases = {
         nds = "nix develop -c $SHELL";
         reload-hm = "nh home switch -b backup ${dotfilesDir}";
-        reload-nd = if pkgs.stdenv.isDarwin then "sudo nh darwin switch ${dotfilesDir}" else "echo 'Did you mean reload-no?'";
-        reload-no = if pkgs.stdenv.isLinux  then "sudo nh os switch ${dotfilesDir}" else "echo 'Did you mean reload-nd?'";
+        reload-nd = if pkgs.stdenv.isDarwin then "nh darwin switch ${dotfilesDir}" else "echo 'Did you mean reload-no?'";
+        reload-no = if pkgs.stdenv.isLinux  then "nh os switch ${dotfilesDir}" else "echo 'Did you mean reload-nd?'";
         reload-config = if pkgs.stdenv.isLinux then "reload-no && reload-hm" else "reload-nd && reload-hm";
       };
     };
