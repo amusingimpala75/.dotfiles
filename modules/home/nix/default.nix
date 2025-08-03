7dfdfd27631a65b9ed30e271dfcfb45e99a6e3f5
amusingimpala75 <69653100+amusingimpala75@users.noreply.github.com>
2025-08-02 22:48:25 -0700

    nh: add env var and allow hiding behind feature flag

--- modules/home/nix/default.nix
+++ modules/home/nix/default.nix
@@ -1,6 +1,10 @@
-{ lib, config, pkgs, dotfilesDir, ... }:
+{ lib, config, dotfilesDir, ... }:
 let
   cfg = config.my.nix;
+  actual-dir =
+    if lib.strings.hasPrefix "~" dotfilesDir
+    then lib.concatStrings [config.home.homeDirectory (builtins.substring 1 (-1) dotfilesDir)]
+    else dotfilesDir;
 in {
   options.my.nix = {
     enable = lib.mkOption {
@@ -9,12 +13,21 @@ in {
       example = true;
       description = "enable nix customization";
     };
+    nh = lib.mkOption {
+      type = lib.types.bool;
+      default = cfg.enable;
+      example = false;
+      description = "enable NH";
+    };
   };
 
   config = lib.mkIf cfg.enable {
-    home = {
-      packages = [ pkgs.nh ];
+    programs.nh = lib.mkIf cfg.nh {
+      enable = true;
+      flake = actual-dir;
+    };
 
+    home = {
       shellAliases = {
         nds = "nix develop -c $SHELL";
         reload-hm = "nh home switch -b backup ${dotfilesDir}";
