95746372b5ffd5faa33b4d5ef6bb1d90258b7c98
amusingimpala75 <69653100+amusingimpala75@users.noreply.github.com>
2026-01-07 15:36:47 -0700

    emacs: fix packages about latex
    final -> efinal: solves the error where it doesn-t aot compile
    ox -> ox-latex: fixes the missing backend error
    the other change fixes the org mode autoloads

--- overlays/emacs-packages.nix
+++ overlays/emacs-packages.nix
@@ -1,10 +1,10 @@
 final: prev: {
   emacsPackagesFor = emacs: ((prev.emacsPackagesFor emacs).overrideScope (efinal: eprev: {
-    nnnrss = final.callPackage ../packages/emacs/nnnrss.nix { };
-    org-modern-indent = final.callPackage ../packages/emacs/org-modern-indent.nix { };
-    toggleterm = final.callPackage ../packages/emacs/toggleterm.nix { };
-    reader = final.callPackage ../packages/emacs/reader.nix { };
-    page-view = final.callPackage ../packages/emacs/page-view.nix { };
-    org-karthik = final.callPackage ../packages/emacs/org-karthik.nix { };
+    nnnrss = efinal.callPackage ../packages/emacs/nnnrss.nix { };
+    org-modern-indent = efinal.callPackage ../packages/emacs/org-modern-indent.nix { };
+    toggleterm = efinal.callPackage ../packages/emacs/toggleterm.nix { };
+    reader = efinal.callPackage ../packages/emacs/reader.nix { };
+    page-view = efinal.callPackage ../packages/emacs/page-view.nix { };
+    org-karthik = efinal.callPackage ../packages/emacs/org-karthik.nix { };
   }));
 }
--- packages/emacs/org-karthik.nix
+++ packages/emacs/org-karthik.nix
@@ -6,17 +6,28 @@
 }:
 let
   epkgs = emacsPackagesFor emacs;
-  rev = "f9f909681a051c73c64cc7b030aa54d70bb78f80";
-  hash = "sha256-MbXBcN6PJ1YSZCyxrg5Fng9pVY9X5FHFB+KAFwQu7zQ=";
+  version = "git+2026-01-05";
+
+  autoloaded-src = epkgs.trivialBuild {
+    inherit version;
+    src = fetchgit {
+      url = "https://code.tecosaur.net/tec/org-mode";
+      rev = "f9f909681a051c73c64cc7b030aa54d70bb78f80";
+      hash = "sha256-MbXBcN6PJ1YSZCyxrg5Fng9pVY9X5FHFB+KAFwQu7zQ=";
+    };
+    pname = "org-karthik-source";
+    buildPhase = ''
+      make autoloads
+    '';
+    installPhase = ''
+      mkdir -p $out
+      cp -R lisp/* doc etc $out/
+    '';
+  };
 in
 epkgs.trivialBuild {
-  pname = "org-karthink";
-  version = "git+2026-1-5";
-  src = fetchgit {
-    url = "https://code.tecosaur.net/tec/org-mode";
-    inherit rev hash;
-  };
+  inherit version;
+  pname = "org-karthik";
+  src = autoloaded-src;
   packageRequires = epkgs.org.packageRequires;
-  sourceRoot = "org-mode-${builtins.substring 0 7 rev}/lisp";
-  # TODO: autoloads
 }
