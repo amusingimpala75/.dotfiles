97ec877b16ced82f96a838e86adf61489916545d
amusingimpala75 <69653100+amusingimpala75@users.noreply.github.com>
2026-01-09 16:52:30 -0700

    emacs+flake: add support for scala with scala-mode and template

---
+++ templates/scala/flake.nix
@@ -0,0 +1,24 @@
+{
+  description = "devshell for scala";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    flake-parts.url = "github:hercules-ci/flake-parts";
+  };
+
+  outputs =
+    inputs@{ flake-parts, ... }:
+    flake-parts.lib.mkFlake { inherit inputs; } {
+      systems = inputs.nixpkgs.lib.systems.flakeExposed;
+      perSystem =
+        { pkgs, ... }:
+        {
+          devShells.default = pkgs.mkShell {
+            packages = with pkgs; [
+              metals
+              sbt
+            ];
+          };
+        };
+    };
+}
