62c07a736716a67002c446bfa8fc19fddea3a13f
amusingimpala75 <69653100+amusingimpala75@users.noreply.github.com>
2026-02-02 15:52:50 -0500

    flake: refactor emacs-packages to use packagesFromDirectoryRecursive
    next up: pkgs-by-name-for-flake-parts

--- overlays/emacs-packages.nix
+++ overlays/emacs-packages.nix
@@ -2,14 +2,9 @@ final: prev: {
   emacsPackagesFor =
     emacs:
     ((prev.emacsPackagesFor emacs).overrideScope (
-      efinal: eprev: {
-        nnnrss = efinal.callPackage ../packages/emacs/nnnrss.nix { };
-        org-modern-indent = efinal.callPackage ../packages/emacs/org-modern-indent.nix { };
-        toggleterm = efinal.callPackage ../packages/emacs/toggleterm.nix { };
-        reader = efinal.callPackage ../packages/emacs/reader.nix { };
-        page-view = efinal.callPackage ../packages/emacs/page-view.nix { };
-        org-karthik = efinal.callPackage ../packages/emacs/org-karthik.nix { };
-        scala-cli-repl = efinal.callPackage ../packages/emacs/scala-cli-repl.nix { };
+      efinal: eprev: final.lib.packagesFromDirectoryRecursive {
+        inherit (efinal) callPackage;
+        directory = ../packages/emacs-packages;
       }
     ));
 }
