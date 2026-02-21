8899c9eb13d308ef40d4b812d9261a3078c0aa4e
amusingimpala75 <69653100+amusingimpala75@users.noreply.github.com>
2026-02-21 16:42:18 -0500

    play-audio: improve output (include composer and title)

--- packages/common/play-audio/package.nix
+++ packages/common/play-audio/package.nix
@@ -1,11 +1,12 @@
 {
   ffmpeg,
+  jaq,
   writeShellApplication,
   ...
 }:
 writeShellApplication {
   name = "play-audio";
   text = builtins.readFile ./play-audio.sh;
-  runtimeInputs = [ ffmpeg ];
+  runtimeInputs = [ ffmpeg jaq ];
   meta.description = "play an audio track (possibly looping) with ffmpeg";
 }
--- packages/common/play-audio/play-audio.sh
+++ packages/common/play-audio/play-audio.sh
@@ -7,14 +7,31 @@ then
 fi
 
 FILE="$1"
-LOOP="$2"
+LOOP=""
 
 if [ $# -lt 2 ]
 then
     LOOP=1
+else
+    LOOP="$2"
 fi
+
+if [ ! -f "$FILE" ]
+then
+    echo "$FILE: file not found"
+    exit 1
+fi
+
+if ! ffprobe "$FILE" >/dev/null 2>&1
+then
+    echo "$FILE: not a valid audio file"
+    exit 1
+fi
+
+ffprobe "$FILE" -v quiet -show_entries format_tags=title,composer -of json |
+    jaq -r '.format.tags | "\(.composer) - \(.title | gsub("_"; " "))"'
 
 for _ in $(seq 1 "$LOOP")
 do
-    ffplay -v 0 -nodisp -autoexit "$FILE"
+    ffplay -nodisp -autoexit "$FILE" >/dev/null 2>&1
 done
