7e1e686d2ee48e61e5b7898ecb9c557a82f59786
amusingimpala75 <69653100+amusingimpala75@users.noreply.github.com>
2026-05-20 13:18:05 -0700

    gha: cleanup by moving package-runner creation to nix expr

---
+++ .github/workflows/package-runner-matrix.nix
@@ -0,0 +1,20 @@
+let
+  flake = (builtins.getFlake ("path:" + (toString ../..)));
+  gen-platform-matrix =
+    { system, runner }:
+    map (package: {
+      inherit package runner;
+    }) (builtins.attrNames flake.packages.${system});
+in
+flake.inputs.nixpkgs.lib.flatten (
+  map gen-platform-matrix [
+    {
+      system = "aarch64-darwin";
+      runner = "macos-latest";
+    }
+    {
+      system = "x86_64-linux";
+      runner = "ubuntu-latest";
+    }
+  ]
+)
