0a830d96a5585b714a243ade5e9122c98af0e18a
amusingimpala75 <69653100+amusingimpala75@users.noreply.github.com>
2026-05-29 19:50:21 -0700

    autowire: move auto template out and remove autowire module
    most functionality had already been removed, and I didn't
    find the app autogen useful (like, what are flake app
    outputs actually useful for?)

---
+++ modules/flake/templates.nix
@@ -0,0 +1,9 @@
+let
+  base = ../../templates;
+in
+{
+  flake.templates = builtins.mapAttrs (key: _: {
+    path = "${base}/${key}";
+    inherit (import "${base}/${key}/flake.nix") description;
+  }) (builtins.readDir base);
+}
