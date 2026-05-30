6c53fb75c0dcd6c77463dfbd3442e1dc51e4808f
amusingimpala75 <69653100+amusingimpala75@users.noreply.github.com>
2026-05-29 19:50:21 -0700

    jujutusu: replace home-manager implementation with wrapper module

--- modules/flake/jujutsu.nix
+++ modules/flake/jujutsu.nix
@@ -1,90 +1,91 @@
 {
-  lib,
+  self,
   ...
 }:
 {
   flake.modules.homeManager.jujutsu =
     {
       config,
-      pkgs,
       ...
     }:
     {
-      programs.jujutsu = {
-        ediff = true;
+      imports = [ self.wrappers.jujutsu.install ];
+      wrappers.jujutsu = {
         enable = true;
-        package =
-          (pkgs.writeShellScriptBin "jj" ''
-            export GIT_CONFIG_GLOBAL=${config.wrappers.custom-git.constructFiles.gitconfig.outPath}
-            exec ${lib.getExe pkgs.jujutsu} "$@"
-          '')
-          // {
-            inherit (pkgs.jujutsu) version;
-          };
-        settings = {
-          aliases = {
-            get-change-ids = [
-              "log"
-              "-G"
-              "-T"
-              "change_id.short() ++ '\n'"
-              "-r"
-            ];
-          };
-          fsmonitor.backend = "watchman";
-          git.private-commits = "visible_heads()";
-          merge-tools.delta.diff-expected-exit-codes = [
-            0
-            1
-          ];
-          merge-tools.weave = {
-            program = "weave-driver";
-            merge-args = [
-              "$base"
-              "$left"
-              "$right"
-              "-o"
-              "$output"
-              "-l"
-              "$marker_length"
-              "-p"
-              "$path"
-            ];
-            merge-conflict-exit-codes = [ 1 ];
-            merge-tool-edits-conflict-markers = true;
-            conflict-marker-style = "git";
-          };
-          revsets = {
-            bookmark-advance-to = "@-";
-          };
-          signing = {
-            backend = "ssh";
-            key = "~/.ssh/id_ed25519.pub";
-            behavior = "drop";
-          };
-          git.sign-on-push = true;
-          ui = {
-            default-command = "log";
-            diff-editor = ":builtin";
-            merge-editor = "weave";
-            show-cryptographic-signatures = true;
-          };
-          user = {
-            name = config.vcs.username;
-            email = config.vcs.email;
-          };
-          working-copy.eol-conversion = "input";
+        settings.user = {
+          name = config.vcs.username;
+          email = config.vcs.email;
         };
       };
-      programs.delta = {
-        enable = true;
-        enableJujutsuIntegration = true;
-        options.syntax-theme = "ansi";
-      };
-      home.packages = with pkgs; [
-        weave
-      ];
       # for other's signatures
       programs.gpg.enable = true;
+    };
+
+  flake.wrappers.jujutsu =
+    {
+      lib,
+      pkgs,
+      wlib,
+      ...
+    }:
+    {
+      imports = [ wlib.wrapperModules.jujutsu ];
+      settings = {
+        aliases = {
+          get-change-ids = [
+            "log"
+            "-G"
+            "-T"
+            "change_id.short() ++ '\n'"
+            "-r"
+          ];
+        };
+        fsmonitor.backend = "watchman";
+        git.private-commits = "visible_heads()";
+        merge-tools.delta.diff-expected-exit-codes = [
+          0
+          1
+        ];
+        merge-tools.weave = {
+          program = lib.getExe' self.packages.${pkgs.stdenv.hostPlatform.system}.weave "weave-driver";
+          merge-args = [
+            "$base"
+            "$left"
+            "$right"
+            "-o"
+            "$output"
+            "-l"
+            "$marker_length"
+            "-p"
+            "$path"
+          ];
+          merge-conflict-exit-codes = [ 1 ];
+          merge-tool-edits-conflict-markers = true;
+          conflict-marker-style = "git";
+        };
+        revsets = {
+          bookmark-advance-to = "@-";
+        };
+        signing = {
+          backend = "ssh";
+          key = "~/.ssh/id_ed25519.pub";
+          behavior = "drop";
+        };
+        git.sign-on-push = true;
+        ui = {
+          default-command = "log";
+          diff-editor = ":builtin";
+          diff-formatter = ":git";
+          merge-editor = "weave";
+          pager = lib.getExe self.packages.${pkgs.stdenv.hostPlatform.system}.delta;
+          show-cryptographic-signatures = true;
+        };
+        working-copy.eol-conversion = "input";
+      };
+
+      env.GIT_CONFIG_GLOBAL = pkgs.writeText "jj-git-config" ''
+        [core]
+          excludesFile = ${self.packages.${pkgs.stdenv.hostPlatform.system}.global-gitignore}
+      '';
     };
 }
