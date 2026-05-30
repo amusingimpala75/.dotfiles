49fea3e061a6e2d815f2555c7e298b210f9ebce2
amusingimpala75 <69653100+amusingimpala75@users.noreply.github.com>
2026-05-29 19:50:21 -0700

    templates: (fix template path) what in the actual
    base + "/" + key = basekey
    base + "/${key}" = base/key
    
    bruh.

--- modules/flake/templates.nix
+++ modules/flake/templates.nix
@@ -3,7 +3,7 @@ let
 in
 {
   flake.templates = builtins.mapAttrs (key: _: {
-    path = base + "/" + key;
+    path = base + "/${key}";
     inherit (import "${base}/${key}/flake.nix") description;
   }) (builtins.readDir base);
 }
