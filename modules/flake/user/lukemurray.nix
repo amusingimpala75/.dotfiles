1b33227d6d7a15991c4554d4ea902f9a420de623
amusingimpala75 <69653100+amusingimpala75@users.noreply.github.com>
2026-07-12 22:18:12 -0700

    darwin: remove dock code, move into user darwin module

--- modules/flake/user/lukemurray.nix
+++ modules/flake/user/lukemurray.nix
@@ -5,6 +5,7 @@
 {
   flake.modules.darwin.lukemurray =
     {
+      config,
       pkgs,
       ...
     }:
@@ -31,6 +32,26 @@
             # tailscale-app
           ]);
       };
+      # [TODO] I long for the day nix-darwin finishes the
+      # migration and I can roll the defaults into the
+      # user definition
+      system.primaryUser = "lukemurray";
+      system.defaults.dock.persistent-apps = [
+        # Finder
+        "/System/Applications/Messages.app"
+        "/System/Applications/Mail.app"
+        "/System/Applications/Maps.app"
+        "/System/Applications/FaceTime.app"
+        "/System/Applications/Calendar.app"
+        "/Applications/Pages.app"
+        "/System/Applications/App Store.app"
+        "/System/Applications/System Settings.app"
+        "${config.users.users.lukemurray.home}/Applications/Home Manager Apps/Firefox.app"
+        "${config.users.users.lukemurray.home}/Applications/Home Manager Apps/Emacs.app"
+        "${config.users.users.lukemurray.home}/Applications/Home Manager Apps/Ghostty.app"
+        "/System/Applications/iPhone Mirroring.app"
+        "${config.users.users.lukemurray.home}/Downloads"
+      ];
     };
 
   flake.homeConfigurations = self.lib.mkHome "aarch64-darwin" "lukemurray" {
@@ -69,53 +90,41 @@
 
     programs.vesktop.enable = true;
 
-    # See both mynixos.com options for nix-darwin and home-manager, as well as macos-defaults.com
-    # Additionally, `defaults read' will list out current settings
-    # Alternately all settings can be found at ~/Library/Preferences/<app-id>.plist
-    targets.darwin.defaults = {
-      "com.apple.dock" = {
-        autohide = true;
-        largesize = 96;
-        magnification = true;
-        mru-spaces = false;
-        orientation = "bottom";
-        show-recents = false;
-      };
-      "com.apple.finder" = {
-        AppleShowAllExtensions = true;
-        AppleShowAllFiles = true; # Requires finder to be killed
-        CreateDesktop = false;
-        FXDefaultSearchScope = "SCcf";
-        FXEnableExtensionChangeWarning = false; # Requires finder to be killed
-        FXPreferredViewStyle = "clmv";
-        FXRemoveOldTrashItems = false;
-        _FXSortFoldersFirst = false;
-        ShowPathbar = true;
-        ShowStatusBar = true;
-      };
-      "NSGlobalDomain" = {
-        # AppleInterfaceStyle # This doesn't seem to work
-        InitialKeyRepeat = 10;
-        KeyRepeat = 1;
+    targets.darwin = {
+      copyApps.enable = true;
+      linkApps.enable = false;
+
+      # See both mynixos.com options for nix-darwin and home-manager, as well as macos-defaults.com
+      # Additionally, `defaults read' will list out current settings
+      # Alternately all settings can be found at ~/Library/Preferences/<app-id>.plist
+      defaults = {
+        "com.apple.dock" = {
+          autohide = true;
+          largesize = 96;
+          magnification = true;
+          mru-spaces = false;
+          orientation = "bottom";
+          show-recents = false;
+        };
+        "com.apple.finder" = {
+          AppleShowAllExtensions = true;
+          AppleShowAllFiles = true; # Requires finder to be killed
+          CreateDesktop = false;
+          FXDefaultSearchScope = "SCcf";
+          FXEnableExtensionChangeWarning = false; # Requires finder to be killed
+          FXPreferredViewStyle = "clmv";
+          FXRemoveOldTrashItems = false;
+          _FXSortFoldersFirst = false;
+          ShowPathbar = true;
+          ShowStatusBar = true;
+        };
+        "NSGlobalDomain" = {
+          # AppleInterfaceStyle # This doesn't seem to work
+          InitialKeyRepeat = 10;
+          KeyRepeat = 1;
+        };
       };
     };
-
-    my.darwin.dock.items = [
-      # Finder
-      "/System/Applications/Messages.app"
-      "/System/Applications/Mail.app"
-      "/System/Applications/Maps.app"
-      "/System/Applications/FaceTime.app"
-      "/System/Applications/Calendar.app"
-      "/Applications/Pages.app"
-      "/System/Applications/App Store.app"
-      "/System/Applications/System Settings.app"
-      "~/Applications/Home Manager Apps/Firefox.app"
-      "~/Applications/Home Manager Apps/Emacs.app"
-      "~/Applications/Home Manager Apps/Ghostty.app"
-      "/System/Applications/iPhone Mirroring.app"
-      "~/Downloads"
-    ];
 
     home.stateVersion = "24.05";
   };
